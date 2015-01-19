b9c415290fe79532520cdcac98590e00dac57c0c
Jaime Caballero <me@jaicab.com>
2015-01-19 18:11:17 +0000

    Logos

--- js/core.js
+++ js/core.js
@@ -1,16 +1,15 @@
+
+
 // @codekit-prepend "vendor/modernizr.js";
 // @codekit-prepend "vendor/polyfiller.js";
 // @codekit-prepend "vendor/picturefill.min.js";
 // @codekit-prepend "vendor/respond.js";
-// @codekit-prepend "vendor/jquery-ui.min.js";
-// @codekit-prepend "vendor/jquery.ui.touch-punch.min.js";
-// @codekit-prepend "vendor/owl.carousel.min.js";
 
 $(function(){
 
 
  	// Makes the tabs work
-    $( ".tabs" ).tabs();
+    //$( ".tabs" ).tabs();
   
   	$(".nav select").change(function() {
 	  window.location = $(this).find("option:selected").val();
