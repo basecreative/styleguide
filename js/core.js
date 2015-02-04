d3566280f55c1b8f53b672de52b818d4cc25fa69
Jaime Caballero <me@jaicab.com>
2015-02-04 12:54:01 +0000

    Atoms... done?

---
+++ js/core.js
@@ -0,0 +1,17 @@
+	console.log("hola");
+// @codekit-prepend "vendor/modernizr.js";
+// @codekit-prepend "vendor/polyfiller.js";
+// @codekit-prepend "vendor/picturefill.min.js";
+// @codekit-prepend "vendor/respond.js";
+
+$(function(){
+
+
+ 	// Makes the tabs work
+    //$( ".tabs" ).tabs();
+  
+  	$(".nav select").change(function() {
+	  window.location = $(this).find("option:selected").val();
+	});
+  
+});
